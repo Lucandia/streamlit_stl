c9ca4a9f6c27002d916b23b4cc29429c996e5edc
Ed Saa <esaavedrac@u.northwestern.edu>
2024-10-07 10:29:59 -0500

    Bump version number

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 setup(
-    name='streamlit_stl',
-    version='0.0.2',
-    author='Luca Monari',
-    author_email='Luca.Monari@mr.mpg.de',
+    name="streamlit_stl",
+    version="0.0.3",
+    author="Luca Monari",
+    author_email="Luca.Monari@mr.mpg.de",
     url="https://github.com/Lucandia/streamlit_stl",
-    description='A Streamlit component to display 3D models in STL format',
+    description="A Streamlit component to display 3D models in STL format",
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
